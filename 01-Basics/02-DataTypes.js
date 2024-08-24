08a7882b4c977e73346f09b9d4aee28e4cadc008
Dipankar Mnadal <dipankar0535@gmail.com>
2024-08-25 01:34:22 +0530

    datatypes

---
+++ 01-Basics/02-DataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; // traet all Js code as newer version
+
+// alert( 3 + 3 ) // we are using nodejs , not browser
+
+
+// premitive datatypes
+
+//string
+let userName = "Dipankar"
+
+//number => 2 to power 53[range]
+let age = 2
+
+//bigint
+
+//boolean => true/false
+let response
+
+response = false
+response = true
+
+//null
+let value = null
+
+//undefined
+let undefinedValue;
+
+//synmbol ==> unique
+
+// type of variables
+console.log(typeof userName)
+console.log(typeof age)  //number
+console.log(typeof response) //boolean
+console.log(typeof value) //object
+console.log(typeof undefinedValue) //undefined
+
